6fc0b4727f2b6b57f87eab424f31b1970211b2e1
umbe96 <umbertolagamba@libero.it>
2019-03-19 15:50:27 +0100

    primo push prova

--- src/main/java/it/contrader/controller/LoginController.java
+++ src/main/java/it/contrader/controller/LoginController.java
@@ -3,11 +3,14 @@ package it.contrader.controller;
 import it.contrader.main.MainDispatcher;
 
 public class LoginController implements Controller {
-
     public LoginController() {
     }
 
     public void doControl (Request request) {
         MainDispatcher.getInstance().callView("Login", request);
     }
+    
+    
+    
+    /*dsjgfdjfdiubfdnfdsnboifdnbobnfdo*/
 }
