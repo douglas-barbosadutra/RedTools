f05678b56c4fca7f091422098f2fd7b0d41e65d1
alfe92 <alfe92@libero.it>
2019-03-20 10:49:11 +0100

    Created BusinessOwner

--- src/main/java/it/contrader/model/BusinessOwner.java
+++ src/main/java/it/contrader/model/BusinessOwner.java
@@ -1,5 +1,75 @@
 package it.contrader.model;
 
-public class BusinessOwner {
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 
+public class BusinessOwner {
+	private int id;
+	private String username;
+	private String password;
+	private String usertype;
+	
+	public BusinessOwner() {
+	}
+
+	public BusinessOwner(String username, String password, String usertype) {
+		this.username = username;
+		this.username = password;
+		this.usertype = usertype;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	 @Override
+	 public String toString() {
+		 return this.getId() + "\t" + this.getUsername() + "\t" + this.getPassword() + "\t" + this.getUsertype();
+	 
+	 }
+	 
+	 public boolean equals(User userCompare) {
+		 if (!this.getUsername().equals(userCompare.getUsername())) {
+			 return false;
+		 }
+		 
+		 if (!this.getPassword().equals(userCompare.getPassword())) {
+				 return false;
+	 }
+		 if (!this.getUsertype().equals(userCompare.getUsertype())) {
+			 return false;
+			 
+		 }
+		 
+		 return true;
+	 }
+		 
 }
+	 
+	 
