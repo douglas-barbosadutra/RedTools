1de3848b8f7f94fde236bca3af0162a76de9bee0
umbe96 <umbertolagamba@libero.it>
2019-03-21 10:23:49 +0100

    insert DTO Dossier

--- src/main/java/it/contrader/dto/DossierDTO.java
+++ src/main/java/it/contrader/dto/DossierDTO.java
@@ -1,5 +1,88 @@
 package it.contrader.dto;
 
 public class DossierDTO {
+	private int costoDipendenti;
+	private double fatturatoPeriodoDiImposta;
+	private int numeroDipendenti;
+	private double costiAttivitaRd;
+	private double costiPersonaleRd;
+	private String partitaIva;
+	private int idProgetto;
+	private int idDossier;
+	private String periodoDiImposta;
+	public DossierDTO(int costoDipendenti, double fatturatoPeriodoDiImposta, int numeroDipendenti,
+			double costiAttivitaRd, double costiPersonaleRd, String partitaIva, int idProgetto, int idDossier,
+			String periodoDiImposta) {
+		super();
+		this.costoDipendenti = costoDipendenti;
+		this.fatturatoPeriodoDiImposta = fatturatoPeriodoDiImposta;
+		this.numeroDipendenti = numeroDipendenti;
+		this.costiAttivitaRd = costiAttivitaRd;
+		this.costiPersonaleRd = costiPersonaleRd;
+		this.partitaIva = partitaIva;
+		this.idProgetto = idProgetto;
+		this.idDossier = idDossier;
+		this.periodoDiImposta = periodoDiImposta;
+	}
+	public int getCostoDipendenti() {
+		return costoDipendenti;
+	}
+	public void setCostoDipendenti(int costoDipendenti) {
+		this.costoDipendenti = costoDipendenti;
+	}
+	public double getFatturatoPeriodoDiImposta() {
+		return fatturatoPeriodoDiImposta;
+	}
+	public void setFatturatoPeriodoDiImposta(double fatturatoPeriodoDiImposta) {
+		this.fatturatoPeriodoDiImposta = fatturatoPeriodoDiImposta;
+	}
+	public int getNumeroDipendenti() {
+		return numeroDipendenti;
+	}
+	public void setNumeroDipendenti(int numeroDipendenti) {
+		this.numeroDipendenti = numeroDipendenti;
+	}
+	public double getCostiAttivitaRd() {
+		return costiAttivitaRd;
+	}
+	public void setCostiAttivitaRd(double costiAttivitaRd) {
+		this.costiAttivitaRd = costiAttivitaRd;
+	}
+	public double getCostiPersonaleRd() {
+		return costiPersonaleRd;
+	}
+	public void setCostiPersonaleRd(double costiPersonaleRd) {
+		this.costiPersonaleRd = costiPersonaleRd;
+	}
+	public String getPartitaIva() {
+		return partitaIva;
+	}
+	public void setPartitaIva(String partitaIva) {
+		this.partitaIva = partitaIva;
+	}
+	public int getIdProgetto() {
+		return idProgetto;
+	}
+	public void setIdProgetto(int idProgetto) {
+		this.idProgetto = idProgetto;
+	}
+	public int getIdDossier() {
+		return idDossier;
+	}
+	public void setIdDossier(int idDossier) {
+		this.idDossier = idDossier;
+	}
+	public String getPeriodoDiImposta() {
+		return periodoDiImposta;
+	}
+	public void setPeriodoDiImposta(String periodoDiImposta) {
+		this.periodoDiImposta = periodoDiImposta;
+	}
+	
+	
+	
+	
+	
+	
 
 }
