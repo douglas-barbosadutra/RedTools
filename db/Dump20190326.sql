e3a17e69a1c69654e8a93e85e317ccdef5a12a32
trevis2 <trevis2@gmail.com>
2019-03-26 18:43:55 +0100

    aggiunta db

---
+++ db/Dump20190326.sql
@@ -0,0 +1,183 @@
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rdtools
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tab_dossier`
+--
+
+DROP TABLE IF EXISTS `tab_dossier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tab_dossier` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `periodo_di_imposta` varchar(4) NOT NULL,
+  `costo_dipendenti_periodo_imposta` double NOT NULL,
+  `fatturato_periodo_di_imposta` double NOT NULL,
+  `numero_totale_dipendenti` int(10) unsigned NOT NULL,
+  `costo_complessivo_attivita` double unsigned NOT NULL,
+  `costo_personale` double unsigned NOT NULL,
+  `id_utente_finale` int(10) unsigned NOT NULL,
+  `id_progetto` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_progetto_idx` (`id_progetto`),
+  KEY `id_utente_finale_idx` (`id_utente_finale`),
+  CONSTRAINT `id_progetto` FOREIGN KEY (`id_progetto`) REFERENCES `tab_progetto` (`idprogetto`),
+  CONSTRAINT `id_utente_finale` FOREIGN KEY (`id_utente_finale`) REFERENCES `tab_utente_finale` (`id_utente_finale`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tab_dossier`
+--
+
+LOCK TABLES `tab_dossier` WRITE;
+/*!40000 ALTER TABLE `tab_dossier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tab_dossier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tab_mom`
+--
+
+DROP TABLE IF EXISTS `tab_mom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tab_mom` (
+  `id_mom` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome_utente_finale_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `luogo_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data_del_giorno_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `orario_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oggetto_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progetto_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `partecipanti_mom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `testo_agenda_mom` longtext COLLATE utf8_unicode_ci,
+  `testo_azione_mom` longtext COLLATE utf8_unicode_ci,
+  `testo_note_mom` longtext COLLATE utf8_unicode_ci,
+  `chiave_est_ut_fin` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_mom`),
+  KEY `chiave_est_ut_fin_idx` (`chiave_est_ut_fin`),
+  CONSTRAINT `chiave_est_ut_fin` FOREIGN KEY (`chiave_est_ut_fin`) REFERENCES `tab_utente_finale` (`id_utente_finale`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tab_mom`
+--
+
+LOCK TABLES `tab_mom` WRITE;
+/*!40000 ALTER TABLE `tab_mom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tab_mom` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tab_progetto`
+--
+
+DROP TABLE IF EXISTS `tab_progetto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tab_progetto` (
+  `idprogetto` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nomeprogetto` varchar(45) NOT NULL,
+  PRIMARY KEY (`idprogetto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tab_progetto`
+--
+
+LOCK TABLES `tab_progetto` WRITE;
+/*!40000 ALTER TABLE `tab_progetto` DISABLE KEYS */;
+INSERT INTO `tab_progetto` VALUES (1,'ReD');
+/*!40000 ALTER TABLE `tab_progetto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tab_user`
+--
+
+DROP TABLE IF EXISTS `tab_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tab_user` (
+  `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `usertype` varchar(45) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tab_user`
+--
+
+LOCK TABLES `tab_user` WRITE;
+/*!40000 ALTER TABLE `tab_user` DISABLE KEYS */;
+INSERT INTO `tab_user` VALUES (1,'admin','admin','superuser'),(2,'ciccio','ciccio','user');
+/*!40000 ALTER TABLE `tab_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tab_utente_finale`
+--
+
+DROP TABLE IF EXISTS `tab_utente_finale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tab_utente_finale` (
+  `denominazione_societa` varchar(100) NOT NULL,
+  `forma_giuridica` varchar(45) NOT NULL,
+  `sede_legale` varchar(45) NOT NULL,
+  `partita_iva` varchar(11) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `e_mail` varchar(45) NOT NULL,
+  `indirizzo_unita_locale` varchar(100) NOT NULL,
+  `attivita_azienda` varchar(45) NOT NULL,
+  `legale_rappresentante` varchar(45) NOT NULL,
+  `nato_a` varchar(45) NOT NULL,
+  `nato_il` varchar(45) NOT NULL,
+  `id_utente` int(10) unsigned NOT NULL,
+  `id_utente_finale` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_utente_finale`),
+  KEY `id_utente_idx` (`id_utente`),
+  CONSTRAINT `id_utente` FOREIGN KEY (`id_utente`) REFERENCES `tab_user` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tab_utente_finale`
+--
+
+LOCK TABLES `tab_utente_finale` WRITE;
+/*!40000 ALTER TABLE `tab_utente_finale` DISABLE KEYS */;
+INSERT INTO `tab_utente_finale` VALUES ('Contrader','s.r.l','benevento','12345678901','123123123','contrader@contrader','via longobardi','ricerca e sviluppo','sabatino ','benevento','01-01-1980',2,1),('qweqwe','srl','asds','12345678902','234234','erwerw','werwer','ricerca','sdfsdf','sdfsd','sdff',2,2);
+/*!40000 ALTER TABLE `tab_utente_finale` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-26 18:42:14
