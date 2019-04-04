5bef8046fc4eb0bf260ed43e26911323f58583f5
VincenzoGiudice <vincenzo.giudice80@gmail.com>
2019-04-04 11:20:27 +0200

    modifiche Progetto ok

--- src/main/java/it/contrader/dao/ProgettoRepository.java
+++ src/main/java/it/contrader/dao/ProgettoRepository.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import it.contrader.model.AziendaCliente;
+import it.contrader.model.Dossier;
+import it.contrader.model.Mom;
 import it.contrader.model.Progetto;
 
 
 public interface ProgettoRepository extends JpaRepository<Progetto, Integer> {
 	
-	public Progetto findProgettoByidProgetto(int idProgetto);
-	public List<Progetto> findAllByidProgetto(int idProgetto);
 	
+	public List<Progetto> findAllByidProgetto(int idProgetto);
+	public List<Progetto> findAllByAziendaCliente(AziendaCliente aziendacliente);
+    public Progetto findByDossier(Dossier dossier);
+    public Progetto findByMom(Mom mom);
 }
 
 
