6dd0f1df3791d22c1b199d48d504af46b5584cd2
VincenzoGiudice <vincenzo.giudice80@gmail.com>
2019-04-04 11:09:58 +0200

    modifica repository e service progetto

--- src/main/java/it/contrader/services/ProgettoService.java
+++ src/main/java/it/contrader/services/ProgettoService.java
@@ -10,73 +10,77 @@ import it.contrader.converter.ConverterProgetto;
 import it.contrader.dao.ProgettoRepository;
 import it.contrader.dto.ProgettoDTO;
 import it.contrader.model.AziendaCliente;
+import it.contrader.model.Dossier;
+import it.contrader.model.Mom;
 import it.contrader.model.Progetto;
 
 
 @Service
 public class ProgettoService {
 	
-	private final ProgettoRepository ProgettoRepository;
+	private final ProgettoRepository progettoRepository;
 	
 	@Autowired
 	public ProgettoService(ProgettoRepository ProgettoRepository) {
-		this.ProgettoRepository = ProgettoRepository;
+		this.progettoRepository = ProgettoRepository;
 	}
 
 	
 	public List<ProgettoDTO> getListaProgettoDTO() {
-		return ConverterProgetto.toListDTO((List<Progetto>) ProgettoRepository.findAll());
+		return ConverterProgetto.toListDTO((List<Progetto>) progettoRepository.findAll());
 	}
 	
 	
 	public ProgettoDTO getProgettoDTOById(Integer id) {
-		return ConverterProgetto.toDTO(ProgettoRepository.findById(id).get());
+		return ConverterProgetto.toDTO(progettoRepository.findById(id).get());
 	}
 	
+	public ProgettoDTO getProgettoDTOByDossier(Dossier dossier) {
+		
+		final Progetto progetto = (Progetto) progettoRepository.findByDossier(dossier);
+		
+		return ConverterProgetto.toDTO(progettoRepository.findByDossier(dossier));
+	}
 	
-	
-	
-
+	public ProgettoDTO getProgettoDTOByMom(Mom mom) {
+		
+		final Progetto progetto = (Progetto) progettoRepository.findByMom(mom);
+		
+		return ConverterProgetto.toDTO(progettoRepository.findByMom(mom));
+	}
 	
 	public boolean insertProgetto(ProgettoDTO ProgettoDTO) {
-		return ProgettoRepository.save(ConverterProgetto.toEntity(ProgettoDTO)) != null;
+		return progettoRepository.save(ConverterProgetto.toEntity(ProgettoDTO)) != null;
 	}
 
 	public boolean updateProgetto(ProgettoDTO ProgettoDTO) {
-		return ProgettoRepository.save(ConverterProgetto.toEntity(ProgettoDTO)) != null;
+		return progettoRepository.save(ConverterProgetto.toEntity(ProgettoDTO)) != null;
 	}
 	
 	public void deleteProgettoByidProgetto(Integer idProgetto) {
-		ProgettoRepository.deleteById(idProgetto);
+		progettoRepository.deleteById(idProgetto);
 	}
 	
 	
-public List<ProgettoDTO> findProgettoDTOByPeriodoDiImposta(String periodoDiImposta) {
+public List<ProgettoDTO> findProgettoDTOByidProgetto(int idProgetto) {
 		
-		
+		final List<Progetto> list = progettoRepository.findAllByidProgetto(idProgetto);
 		final List<ProgettoDTO> ProgettoDTOs = new ArrayList<>();
-		
+		list.forEach(i -> ProgettoDTOs.add(ConverterProgetto.toDTO(i)));
 		return ProgettoDTOs;
+
+		}
+
+public List<ProgettoDTO> findProgettoDTOByAziendaCliente(AziendaCliente aziendacliente) {
 	
+	final List<Progetto> list = progettoRepository.findAllByAziendaCliente(aziendacliente);
+	final List<ProgettoDTO> ProgettoDTOs = new ArrayList<>();
+	list.forEach(i -> ProgettoDTOs.add(ConverterProgetto.toDTO(i)));
+	return ProgettoDTOs;
+
 	}
 
-public List<ProgettoDTO> findProgettoDTOByProgetto(Progetto progetto) {
-	
-//	final List<Progetto> list = ProgettoRepository.findAllByidProgetto(idprogetto);
-	final List<ProgettoDTO> ProgettoDTOs = new ArrayList<>();
-//	list.forEach(i -> ProgettoDTOs.add(ConverterProgetto.toDTO(i)));
-	return ProgettoDTOs;
 
-}
-
-public List<ProgettoDTO> findProgettoDTOByAziendaCliente(AziendaCliente aziendaCliente) {
-	
-	//final List<Progetto> list = ProgettoRepository.findAllByAziendaCliente(aziendaCliente);
-	final List<ProgettoDTO> ProgettoDTOs = new ArrayList<>();
-//	list.forEach(i -> ProgettoDTOs.add(ConverterProgetto.toDTO(i)));
-	return ProgettoDTOs;
-
-}
 
 }
 
