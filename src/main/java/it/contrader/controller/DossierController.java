b383af59e35a0799a2d05d3b3a696620d33ed050
Leonardo Lisanti <l.lisanti@ivert.it>
2019-04-13 21:07:28 +0200

    added classpath to use relative path

--- src/main/java/it/contrader/controller/DossierController.java
+++ src/main/java/it/contrader/controller/DossierController.java
@@ -24,6 +24,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -166,7 +167,6 @@ public class DossierController {
 	public void getFile(
 	    @PathVariable("file_name") String fileName, 
 	    HttpServletResponse response, HttpServletRequest request) {
-		String path="C:\\Users\\Contrader\\Desktop\\Contrader\\RedToolSpring\\src\\main\\resources\\files\\";
 		int idProgetto = (int) session.getAttribute("idProgetto");
 		Progetto progetto = progettoService.getProgettoById(idProgetto);
 		
@@ -182,143 +182,139 @@ public class DossierController {
 		
 		try {
 
-		    Workbook workbook;
-		    workbook = new XSSFWorkbook(
-		        OPCPackage.open(path+"docTables.xlsm")
-		    );
+			File sourceFile = ResourceUtils.getFile("classpath:files\\docTables.xlsm");
+			InputStream sourceIs = new FileInputStream(sourceFile);
 
-		    //DO STUF WITH WORKBOOK
-		    Sheet sheet = workbook.getSheetAt(0);
-		    Cell cell2Update = sheet.getRow(2).getCell(2);
-		    cell2Update.setCellValue(dossier.getPeriodoDiImposta());
-		    cell2Update = sheet.getRow(4).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getDenominazioneSocieta());
-		    cell2Update = sheet.getRow(5).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getFormaGiuridica());
-		    cell2Update = sheet.getRow(7).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getSedeLegale());
-		    cell2Update = sheet.getRow(8).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getPartitaIva());
-		    cell2Update = sheet.getRow(9).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getTelefono());
-		    cell2Update = sheet.getRow(10).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getEmail());
-		    cell2Update = sheet.getRow(12).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getIndirizzoUnitaLocale());
-		    cell2Update = sheet.getRow(14).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getAttivitaAzienda());
-		    cell2Update = sheet.getRow(16).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getLegaleRappresentante());
-		    cell2Update = sheet.getRow(17).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getNatoA());
-		    cell2Update = sheet.getRow(18).getCell(2);
-		    cell2Update.setCellValue(aziendaCliente.getNatoIl());
-		    cell2Update = sheet.getRow(20).getCell(2);
-		    cell2Update.setCellValue(dossier.getCostoDipendentiPeriodoDiImposta());
-		    cell2Update = sheet.getRow(21).getCell(2);
-		    cell2Update.setCellValue(dossier.getFatturatoPeriodoDiImposta());
-		    cell2Update = sheet.getRow(22).getCell(2);
-		    cell2Update.setCellValue(dossier.getNumeroTotaleDipendenti());
-		    cell2Update = sheet.getRow(24).getCell(2);
-		    double sum = dossier.getTotaleCostiReD() + dossier.getCostiPersonaleReD();
-		    cell2Update.setCellValue(sum);
-		    cell2Update = sheet.getRow(25).getCell(2);
-		    cell2Update.setCellValue(dossier.getCostiPersonaleReD());
-		    cell2Update = sheet.getRow(27).getCell(2);
-		    cell2Update.setCellValue(progetto.getTitoloProgetto());
-		    cell2Update = sheet.getRow(28).getCell(2);
-		    cell2Update.setCellValue(progetto.getDettagliProgetto());
-		    cell2Update = sheet.getRow(29).getCell(2);
-		    cell2Update.setCellValue(progetto.getCoordinateDIIN());
-		    cell2Update = sheet.getRow(31).getCell(2);
-		    cell2Update.setCellValue(dossier.getTotaleAmmissibile());
-		    
-		    sheet = workbook.getSheetAt(1);
-		    
-		    int i=4;
-		    for (FatturaDTO fattura : allFattura) {
-		    	
-		    	cell2Update = sheet.getRow(i).getCell(2);
-			    cell2Update.setCellValue(fattura.getFornitore().getNomeFornitore());
-			    cell2Update = sheet.getRow(i).getCell(3);
-			    cell2Update.setCellValue(fattura.getDataFattura());
-			    cell2Update = sheet.getRow(i).getCell(4);
-			    cell2Update.setCellValue(fattura.getNumeroFattura());
-			    cell2Update = sheet.getRow(i).getCell(5);
-			    cell2Update.setCellValue(fattura.getDescrizione());
-			    cell2Update = sheet.getRow(i).getCell(6);
-			    cell2Update.setCellValue(fattura.getTotaleImponibile());
-			    cell2Update = sheet.getRow(i).getCell(7);
-			    cell2Update.setCellValue(fattura.getPercentualeAmmissibile());
-			    cell2Update = sheet.getRow(i).getCell(8);
-			    cell2Update.setCellValue(fattura.getTotaleAmmissibile());
-			    i++; 
+			Workbook workbook;
+			workbook = new XSSFWorkbook(OPCPackage.open(sourceIs));
+
+			// DO STUF WITH WORKBOOK
+			Sheet sheet = workbook.getSheetAt(0);
+			Cell cell2Update = sheet.getRow(2).getCell(2);
+			cell2Update.setCellValue(dossier.getPeriodoDiImposta());
+			cell2Update = sheet.getRow(4).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getDenominazioneSocieta());
+			cell2Update = sheet.getRow(5).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getFormaGiuridica());
+			cell2Update = sheet.getRow(7).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getSedeLegale());
+			cell2Update = sheet.getRow(8).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getPartitaIva());
+			cell2Update = sheet.getRow(9).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getTelefono());
+			cell2Update = sheet.getRow(10).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getEmail());
+			cell2Update = sheet.getRow(12).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getIndirizzoUnitaLocale());
+			cell2Update = sheet.getRow(14).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getAttivitaAzienda());
+			cell2Update = sheet.getRow(16).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getLegaleRappresentante());
+			cell2Update = sheet.getRow(17).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getNatoA());
+			cell2Update = sheet.getRow(18).getCell(2);
+			cell2Update.setCellValue(aziendaCliente.getNatoIl());
+			cell2Update = sheet.getRow(20).getCell(2);
+			cell2Update.setCellValue(dossier.getCostoDipendentiPeriodoDiImposta());
+			cell2Update = sheet.getRow(21).getCell(2);
+			cell2Update.setCellValue(dossier.getFatturatoPeriodoDiImposta());
+			cell2Update = sheet.getRow(22).getCell(2);
+			cell2Update.setCellValue(dossier.getNumeroTotaleDipendenti());
+			cell2Update = sheet.getRow(24).getCell(2);
+			double sum = dossier.getTotaleCostiReD() + dossier.getCostiPersonaleReD();
+			cell2Update.setCellValue(sum);
+			cell2Update = sheet.getRow(25).getCell(2);
+			cell2Update.setCellValue(dossier.getCostiPersonaleReD());
+			cell2Update = sheet.getRow(27).getCell(2);
+			cell2Update.setCellValue(progetto.getTitoloProgetto());
+			cell2Update = sheet.getRow(28).getCell(2);
+			cell2Update.setCellValue(progetto.getDettagliProgetto());
+			cell2Update = sheet.getRow(29).getCell(2);
+			cell2Update.setCellValue(progetto.getCoordinateDIIN());
+			cell2Update = sheet.getRow(31).getCell(2);
+			cell2Update.setCellValue(dossier.getTotaleAmmissibile());
+
+			sheet = workbook.getSheetAt(1);
+
+			int i = 4;
+			for (FatturaDTO fattura : allFattura) {
+
+				cell2Update = sheet.getRow(i).getCell(2);
+				cell2Update.setCellValue(fattura.getFornitore().getNomeFornitore());
+				cell2Update = sheet.getRow(i).getCell(3);
+				cell2Update.setCellValue(fattura.getDataFattura());
+				cell2Update = sheet.getRow(i).getCell(4);
+				cell2Update.setCellValue(fattura.getNumeroFattura());
+				cell2Update = sheet.getRow(i).getCell(5);
+				cell2Update.setCellValue(fattura.getDescrizione());
+				cell2Update = sheet.getRow(i).getCell(6);
+				cell2Update.setCellValue(fattura.getTotaleImponibile());
+				cell2Update = sheet.getRow(i).getCell(7);
+				cell2Update.setCellValue(fattura.getPercentualeAmmissibile());
+				cell2Update = sheet.getRow(i).getCell(8);
+				cell2Update.setCellValue(fattura.getTotaleAmmissibile());
+				i++;
 			}
 
-		    if (allFattura.size()>0 && allFattura.get(0) != null) {
-		    	cell2Update = sheet.getRow(8).getCell(8);
-		    	cell2Update.setCellValue(allFattura.get(0).getDossier().getTotaleAmmissibile());
-		    	
-		    	cell2Update = sheet.getRow(10).getCell(8);
-		    	cell2Update.setCellValue(allFattura.get(0).getDossier().getNumeroFornitori());
-		    	
-		    }
-		    
-		    sheet = workbook.getSheetAt(2);
-		    
-		    i=3;
+			if (allFattura.size() > 0 && allFattura.get(0) != null) {
+				cell2Update = sheet.getRow(8).getCell(8);
+				cell2Update.setCellValue(allFattura.get(0).getDossier().getTotaleAmmissibile());
+
+				cell2Update = sheet.getRow(10).getCell(8);
+				cell2Update.setCellValue(allFattura.get(0).getDossier().getNumeroFornitori());
+
+			}
+
+			sheet = workbook.getSheetAt(2);
+
+			i = 3;
 			for (TotaleOreReDDTO totaleOreReD : allTotaleOreReDDTO) {
-				
-				
+
 				cell2Update = sheet.getRow(i).getCell(12);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getNominativo());
-			    cell2Update = sheet.getRow(i).getCell(13);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getLivello());
-			    cell2Update = sheet.getRow(i).getCell(14);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getQualifica());
-			    cell2Update = sheet.getRow(i).getCell(15);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getMansione());
-			    cell2Update = sheet.getRow(i).getCell(16);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getTitoloDiStudio());
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getNominativo());
+				cell2Update = sheet.getRow(i).getCell(13);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getLivello());
+				cell2Update = sheet.getRow(i).getCell(14);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getQualifica());
+				cell2Update = sheet.getRow(i).getCell(15);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getMansione());
+				cell2Update = sheet.getRow(i).getCell(16);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getTitoloDiStudio());
 //			    cell2Update = sheet.getRow(i).getCell(17);
 //			    cell2Update.setCellValue(totaleOreReD.getOreLavorateRed());
 //			    cell2Update = sheet.getRow(i).getCell(18);
 //			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getCostoOrario());
 //			    cell2Update = sheet.getRow(i).getCell(19);
 //			    cell2Update.setCellValue(totaleOreReD.getTotaleCostiReD());
-			    cell2Update = sheet.getRow(i).getCell(7);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getTotaleOreLavorate());
-			    cell2Update = sheet.getRow(i).getCell(8);
-			    cell2Update.setCellValue(totaleOreReD.getImpiegato().getCostoLordoAnnuo());
+				cell2Update = sheet.getRow(i).getCell(7);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getTotaleOreLavorate());
+				cell2Update = sheet.getRow(i).getCell(8);
+				cell2Update.setCellValue(totaleOreReD.getImpiegato().getCostoLordoAnnuo());
 				i++;
 			}
-			    FileOutputStream out = new FileOutputStream(new File(path+fileName));
-		    workbook.write(out);
-		    out.close();
-		    System.out.println("xlsm created successfully..");
+			File resultFile = ResourceUtils.getFile("classpath:files\\" + fileName);
 
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
+			FileOutputStream out = new FileOutputStream(resultFile);
+			workbook.write(out);
+			out.close();
+			System.out.println("xlsm created successfully..");
+
+			// file download
+
+			// get your file as InputStream
+			InputStream is = new FileInputStream(resultFile);
+			// copy it to response's OutputStream
+
+			String type = "vnd.ms-excel";
+			response.setHeader("content-disposition", "attachment; filename=mod_docTables.xlsm");
+			response.setContentType(type);
+			org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+			response.flushBuffer();
+		} catch (IOException ex) {
+			GestoreEccezioni.getInstance().gestisciEccezione(ex);
 		} catch (InvalidFormatException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		
-		// file download 
-	    try {
-	      // get your file as InputStream
-	      InputStream is = new FileInputStream(path+fileName);
-	      // copy it to response's OutputStream
-	      
-	      String type = "vnd.ms-excel";
-	      response.setHeader("content-disposition", "attachment; filename=mod_docTables.xlsm");
-	      response.setContentType(type);
-	      org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
-	      response.flushBuffer();
-	    } catch (IOException ex) {
-	    	GestoreEccezioni.getInstance().gestisciEccezione(ex);
-	    }
 
 	}	
 	@RequestMapping(value = "/visualizzaCostiEsterni", method = RequestMethod.GET)
