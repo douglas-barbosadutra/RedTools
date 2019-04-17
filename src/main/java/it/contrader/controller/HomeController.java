5afefcb121be84e620f4416c9cfbcd51dfaafdd1
trevis2 <trevis2@gmail.com>
2019-04-17 15:24:37 +0200

    modifiche home controller

--- src/main/java/it/contrader/controller/HomeController.java
+++ src/main/java/it/contrader/controller/HomeController.java
@@ -6,57 +6,55 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import it.contrader.dto.AziendaClienteDTO;
 import it.contrader.dto.UserDTO;
+import it.contrader.services.AziendaClienteService;
 import it.contrader.services.UserService;
 
-@Controller
+@CrossOrigin
+@RestController
 @RequestMapping("/HomeController")
 public class HomeController {
 
 	private final UserService userService;
+	private final AziendaClienteService aziendaClienteService;
 	
 
 	@Autowired
-	public HomeController(UserService userService) {
+	public HomeController(UserService userService, AziendaClienteService aziendaClienteService ) {
 		this.userService = userService;
+		this.aziendaClienteService = aziendaClienteService;
 	}
 
-	@RequestMapping(value = "/BoManagement", method = RequestMethod.GET)
-	public String chatManagement(HttpServletRequest request) {
-		return "homeBO";
-
-	}
-
-	@RequestMapping(value = "/userManage", method = RequestMethod.GET)
-	public String userManagement(HttpServletRequest request) {
+	@RequestMapping(value = "/homeAdmin", method = RequestMethod.GET)
+	public String homeAdmin(HttpServletRequest request) {
 		List<UserDTO> allUser = this.userService.getListaUserDTO();
 		request.setAttribute("allUserDTO", allUser);
 		return "homeAdmin";
-
+	}
+	
+	@RequestMapping(value = "/homeBO", method = RequestMethod.GET)
+	public String homeBO(HttpServletRequest request) {
+		int idBO = (int) request.getAttribute("idBO");
+		List<AziendaClienteDTO> allAziendaCliente = this.aziendaClienteService.findAziendeClientiDTOByIdUser(idBO);
+		request.setAttribute("allAziendaClienteDTO", allAziendaCliente);
+		return "homeBO";
 	}
 
-//	dà errore se si toglie il commento e non penso sia corretto inserirlo in questo punto
-//	@RequestMapping(value = "/dossierManage", method = RequestMethod.GET)
-//	public String dossierManage(HttpServletRequest request) {
-//		List<DossierDTO> allDossier = this.dossierService.getListaDossierDTO();
-//		request.setAttribute("allDossierDTO", allDossier);
-//		return "manageAziendaCliente";
-//
-//	}
-
-	
-	
+	@RequestMapping(value = "/homeCliente", method = RequestMethod.GET)
+	public String homeCliente(HttpServletRequest request) {
+		return "homeCliente";
+	}
+			
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpServletRequest request) {
 		return "index";
 
 	}
-	@RequestMapping(value = "/indietro", method = RequestMethod.GET)
-	public String indietro(HttpServletRequest request) {
-		return "home";
-
-	}
+	
 }
